b860af94fd6ba4c07be095a8d0696faf844ecc91
zlxxo <mramic2@etf.unsa.ba>
2020-11-21 18:05:00 +0100

    spirala 2 zadatak 1 - provjeravanje vremena rasporeda i ispisivanje teksta u okviru u slucaju da je vrijeme netacno

--- raspored/iscrtaj.js
+++ raspored/iscrtaj.js
@@ -1,5 +1,8 @@
 function iscrtajRaspored(div, dani, satPocetak, satKraj) {
-    alert("povezano");
+    if(satPocetak >= satKraj || satPocetak < 0 || satPocetak > 24 || satKraj < 0 ||
+        satKraj > 24 || !Number.isInteger(satPocetak) || !Number.isInteger(satKraj)) {
+        div.innerHTML += "Greška";
+    }
 }
 
 function dodajAktivnost(raspored, naziv, tip, vrijemePocetak, vrijemeKraj, dan) {
