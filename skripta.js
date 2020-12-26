8d77ba4fe1d35eae34fe6d2c378fe2e51671c14b
zlxxo <mramic2@etf.unsa.ba>
2020-12-26 18:20:22 +0100

    zadatak 2 - zapoceo rute

--- skripta.js
+++ skripta.js
@@ -11,5 +11,22 @@ app.use('', express.static(__dirname + '/iscrtavanje'));
 app.use('', express.static(__dirname + '/podaciStudent'));
 app.use('', express.static(__dirname + '/raspored'));*/
 
+
+app.get('/predmeti', function (req, res) {});
+
+app.get('/aktivnosti', function (req, res) {});
+
+app.get('/predmet/:naziv/aktivnost/', function (req, res) {});
+
+app.post('/predmet', function (req, res) {});
+
+app.post('/aktivnost', function (req, res) {});
+
+app.delete('/aktivnost/:naziv', function (req, res) {});
+
+app.delete('/predmet/:naziv', function (req, res) {});
+
+app.delete('/all', function (req, res) {});
+
 // make the server listen to requests
 app.listen(PORT);
