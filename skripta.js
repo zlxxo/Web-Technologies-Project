2e01923a1dc10142537bf2860d51cd6b55545a1f
zlxxo <mramic2@etf.unsa.ba>
2020-12-28 18:39:19 +0100

    zadatak 2 - post metoda /predmet - treba uraditi uzimanje naziva iz zahtjeva

--- skripta.js
+++ skripta.js
@@ -1,30 +1,31 @@
-const express = require("express");
+const express = require('express');
 var app = express();
 var path = require('path');
+var fs = require('fs');
 const PORT = 3000;
 
 // pristup statickim fajlovima preko servera
 app.use(express.static(__dirname + '/'));
 
 // rute
-var predmeti = [{naziv: "WT"},
-                {naziv: "RMA"},
-                {naziv: "RG"},
-                {naziv: "DM"},
-                {naziv: "OOI"},
-                {naziv: "OIS"},
-                {naziv: "VVS"}];
+var predmeti = [{naziv:"WT"},
+                {naziv:"RMA"},
+                {naziv:"RG"},
+                {naziv:"DM"},
+                {naziv:"OOI"},
+                {naziv:"OIS"},
+                {naziv:"VVS"}];
 app.get('/predmeti', function (req, res) {
     res.send(JSON.stringify(predmeti));
 });
 
-var aktivnosti = [{naziv: "WT", tip: "predavanje", pocetak: 9, kraj: 12, dan: "Ponedjeljak"},
-                  {naziv: "WT", tip: "vježbe", pocetak: 12, kraj: 14, dan: "Ponedjeljak"},
-                  {naziv: "RMA", tip: "predavanje", pocetak: 14, kraj: 17, dan: "Ponedjeljak"},
-                  {naziv: "RMA", tip: "vježbe", pocetak: 12, kraj: 14, dan: "Utorak"},
-                  {naziv: "DM", tip: "tutorijal", pocetak: 14, kraj: 16, dan: "Utorak"},
-                  {naziv: "DM", tip: "predavanje", pocetak: 16, kraj: 18, dan: "Utorak"},
-                  {naziv: "OI", tip: "predavanje", pocetak: 12, kraj: 15, dan: "Srijeda"}];
+var aktivnosti = [{naziv:"WT",tip:"predavanje",pocetak:9,kraj:12,dan:"Ponedjeljak"},
+                  {naziv:"WT",tip:"vježbe",pocetak:12,kraj:14,dan:"Ponedjeljak"},
+                  {naziv:"RMA",tip:"predavanje",pocetak:14,kraj:17,dan:"Ponedjeljak"},
+                  {naziv:"RMA",tip:"vježbe",pocetak:12,kraj:14,dan:"Utorak"},
+                  {naziv:"DM",tip:"tutorijal",pocetak:14,kraj:16,dan:"Utorak"},
+                  {naziv:"DM",tip:"predavanje",pocetak:16,kraj:18,dan:"Utorak"},
+                  {naziv:"OI",tip:"predavanje",pocetak:12,kraj: 15,dan:"Srijeda"}];
 
 app.get('/aktivnosti', function (req, res) {
     res.send(JSON.stringify(aktivnosti));
@@ -42,7 +43,30 @@ app.get('/predmet/:naziv/aktivnost/', function (req, res) {
     res.send(JSON.stringify(aktivnosti_predmeta));
 });
 
-app.post('/predmet', function (req, res) {});
+app.post('/predmet', function (req, res) {
+    let tijelo = req.body;
+    let predmet = "RMA"; //tijelo['naziv'];
+    let novaLinija = predmet + "\n";
+    fs.readFile('resursi/predmeti.txt', function(err, buffer) {
+        if(err) throw err;
+        var procitano = buffer.toString("utf-8");
+        var arr = procitano.split("\n");
+        let postoji = 0;
+        arr.forEach(element => {
+            if(element == predmet) {
+                postoji++;
+            }
+        });
+        if(postoji > 0) {
+            res.json({message:"Naziv predmeta postoji!"});
+            return;
+        }
+        fs.appendFile('resursi/predmeti.txt', novaLinija, function(err) {
+            if(err) throw err;
+            res.json({message:"Uspješno dodan predmet!"});
+        });
+    });
+});
 
 app.post('/aktivnost', function (req, res) {});
 
