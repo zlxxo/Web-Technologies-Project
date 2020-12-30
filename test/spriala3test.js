ec34b80c2f3dd3612e13b4dfab72f212f5279499
zlxxo <mramic2@etf.unsa.ba>
2020-12-30 13:18:26 +0100

    spirala 3 zadatak 3 - citanje testnih slucajeva iz fajla

--- test/spriala3test.js
+++ test/spriala3test.js
@@ -4,7 +4,30 @@ var fs = require('fs');
 const PORT = 3000;
 
 describe('Testiranje - spirala 3', function() {
-    it('', function() {
-        assert.equal(-1, -1);
+    fs.readFile('resursi/testniPodaci.txt', function(err, buffer) {
+        if(err) throw err;
+        var procitano = buffer.toString("utf-8");
+        var arr = procitano.split("\n");
+        for(let i = 0; i < arr.length; i++) {
+            var element = arr[i];
+            if(element != "") {
+                var linija = element.split(";");
+                var operacija = linija[0];
+                var ruta = linija[1];
+                var ulaz = linija[2];
+                var izlaz = linija[3];
+                var s = operacija + "," + ruta + "," + ulaz + "," + izlaz;
+                describe(s, function() {
+                    it(s, function() {
+                        assert.equal(-1, -1);
+                    });
+                });
+            }
+        }
+    });
+
+    describe('potrebno da bi se mogli pokrenuti testovi procitani iz fajla', function() {
+        it('potrebno da bi se mogli pokrenuti testovi procitani iz fajla', function() {
+        });
     });
 });
