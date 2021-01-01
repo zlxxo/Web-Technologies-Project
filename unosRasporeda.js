2185634214c426ab86590abad61c021dd9e1b0cf
zlxxo <mramic2@etf.unsa.ba>
2021-01-01 16:15:16 +0100

    spirala 3 zadatak 4 - zapoceo citanje podataka iz fajlova

--- unosRasporeda.js
+++ unosRasporeda.js
@@ -1,3 +1,28 @@
+const request = require('request');
+const PORT = 3000;
+
 function unesiRaspored() {
-    alert("radi");
+    let predmeti = [];
+    let aktivnost = [];
+    try {
+        predmeti = posaljiZahtjev("GET", "/predmeti", {});
+        alert(predmeti);
+    } catch(err) {
+        alert("Ne mogu se pročitati predmeti iz fajla!");
+    }
+}
+
+function posaljiZahtjev(operacija, ruta, ulaz) {
+    let options = {
+        "method": operacija,
+        "url": 'http://localhost:' + PORT + ruta,
+        "headers": ulaz
+    };
+    let izlaz = null;
+    request(options, function (error, response) {
+        alert(response);
+        if (error) throw new Error(error);
+        izlaz = response;
+    });
+    return izlaz;
 }
