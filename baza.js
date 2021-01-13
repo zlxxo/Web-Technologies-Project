3348f4fbe77bfaaa0d8f065297f8c7d939a38f75
zlxxo <mramic2@etf.unsa.ba>
2021-01-13 16:20:19 +0100

    spirala 4 zadatak 1 - modeli, relacije i kreiranje baze

--- baza.js
+++ baza.js
@@ -20,4 +20,29 @@ baza.Student = Student;
 const Tip = require('./modeli/tip.js')(sequelize);
 baza.Tip = Tip;
 
+// relacije
+// Predmet 1 - N Grupa
+baza.Predmet.belongsTo(baza.Grupa);
+baza.Grupa.hasMany(baza.Predmet, {as: 'predmeti'});
+
+// Aktivnost N - 1 Predmet
+baza.Aktivnost.belongsTo(baza.Predmet);
+baza.Predmet.hasMany(baza.Aktivnost, {as:'aktivnostiPredmeta'});
+
+// Aktivnost N - 0 Grupa
+baza.Aktivnost.belongsTo(baza.Grupa);
+baza.Grupa.hasMany(baza.Aktivnost, {as:'aktivnostiGrupa'});
+
+// Aktivnost N - 1 Dan
+baza.Aktivnost.belongsTo(baza.Dan);
+baza.Dan.hasMany(baza.Aktivnost, {as: 'aktivnostiUDanu'});
+
+// Aktivnost N - 1 Tip
+baza.Aktivnost.belongsTo(baza.Tip);
+baza.Tip.hasMany(baza.Aktivnost, {as: 'tipoviAktivnosti'});
+
+// Student N - M Grupa
+baza.StudentskeGrupe = baza.Student.belongsToMany(baza.Grupa, {through:'StudentskeGrupe'});
+baza.Grupa.belongsToMany(baza.Student, {through:'StudentskeGrupe'});
+
 module.exports = baza;
