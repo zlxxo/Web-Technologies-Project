91761431c108be35eade91aedd16b032c5c67f2c
zlxxo <mramic2@etf.unsa.ba>
2021-01-14 14:37:22 +0100

    spirala 4 zadatak 2 - ocitavanje grupe

--- studenti.js
+++ studenti.js
@@ -5,7 +5,14 @@ window.onload = () => {
     xhr.withCredentials = true;
     xhr.addEventListener("readystatechange", function() {
         if(this.readyState === 4) {
-            grupe = this.responseText;
+            grupe = JSON.parse(this.responseText);
+            let gr = document.getElementById("grupe");
+            let opcije = "";
+            for(let i = 0; i < grupe.length; i++) {
+                let grupa = grupe[i];
+                opcije += "<option id='" + grupa.id + "'>" + grupa.naziv + "</option>";
+            }
+            gr.innerHTML += opcije;
         }
     });
     xhr.open("GET", "http://localhost:3000/v2/grupa");
