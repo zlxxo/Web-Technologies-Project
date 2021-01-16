37457a001fc8bc38f3038d2e9d5f92e8bbdaab68
zlxxo <mramic2@etf.unsa.ba>
2021-01-16 20:01:05 +0100

    spirala 4 zadatak 3 - upis predmeta i aktivnosti u bazu

--- unosRasporeda.js
+++ unosRasporeda.js
@@ -9,6 +9,7 @@ window.onload = () => {
     xhr.addEventListener("readystatechange", function() {
         if(this.readyState === 4) {
             predmeti = JSON.parse(this.responseText);
+            //alert(JSON.stringify(predmeti));
         }
     });
     xhr.open("GET", "http://localhost:3000/v2/predmet");
@@ -19,6 +20,7 @@ window.onload = () => {
     xhr2.addEventListener("readystatechange", function() {
         if(this.readyState === 4) {
             aktivnosti = JSON.parse(this.responseText);
+            //alert(JSON.stringify(aktivnosti));
         }
     });
     xhr2.open("GET", "http://localhost:3000/v2/aktivnost");
@@ -29,6 +31,7 @@ window.onload = () => {
     xhr3.addEventListener("readystatechange", function() {
         if(this.readyState === 4) {
             tipovi = JSON.parse(this.responseText);
+            //alert(JSON.stringify(tipovi));
             let tp = document.getElementById("tip");
             let opcije = "";
             for(let i = 0; i < tipovi.length; i++) {
@@ -46,6 +49,7 @@ window.onload = () => {
     xhr4.addEventListener("readystatechange", function() {
         if(this.readyState === 4) {
             dani = JSON.parse(this.responseText);
+            //alert(JSON.stringify(dani));
             let dn = document.getElementById("dan");
             let opcije = "";
             for(let i = 0; i < dani.length; i++) {
@@ -62,21 +66,21 @@ window.onload = () => {
 
 function unesiRaspored() {
     let naziv = document.getElementById('naziv-predmeta').value;
-    alert(naziv);
+    //alert(naziv);
     let tp = document.getElementById("tip");
     let tipId = tp.options[tp.selectedIndex].id;
     let tip = pronadjiTip(tipId);
-    alert(JSON.stringify(tip));
+    //alert(JSON.stringify(tip));
     let pocetak = document.getElementById("vrijeme-pocetka").value;
-    alert(pocetak);
+    //alert(pocetak);
     let kraj = document.getElementById("vrijeme-kraja").value;
-    alert(kraj);
+    //alert(kraj);
     let dn = document.getElementById("dan");
     let danId = dn.options[dn.selectedIndex].id;
     let dan = pronadjiDan(danId);
-    alert(JSON.stringify(dan));
+    //alert(JSON.stringify(dan));
 
-    /*if(naziv != null && naziv != "" && tip != null && pocetak != null && kraj != null && dan != null) {
+    if(naziv != null && naziv != "" && tip != null && pocetak != null && kraj != null && dan != null) {
         if(!(pocetak[3] == 3 || pocetak[3] == 0) || pocetak[4] != 0) {
             alert("Vrijeme početka aktivnosti nije u ispravnom formatu!");
             return;
@@ -100,25 +104,38 @@ function unesiRaspored() {
         }
 
         let greska = false;
-        if(!predmeti.includes(naziv)) {
+
+        let pr = pronadjiPredmet(naziv);
+
+        if(pr == null) {
             var data = JSON.stringify({naziv:naziv});
             var xhr = new XMLHttpRequest();
             xhr.withCredentials = true;
             xhr.addEventListener("readystatechange", function() {
                 if(this.readyState === 4) {
-                    if(this.responseText != JSON.stringify({message:"Uspješno dodan predmet!"})) {
+                    if(this.responseText == "Predmet već upisan!") {
                         greska = true;
                         alert("Greška! Neuspješno upisivanje novog predmeta!");
+                    } else {
+                        pr = JSON.parse(this.responseText);
                     }
                 }
             });
-            xhr.open("POST", "http://localhost:3000/v1/predmet");
+            xhr.open("POST", "http://localhost:3000/v2/predmet");
             xhr.setRequestHeader("Content-Type", "application/json");
             xhr.send(data);
-        }
 
+        }
+        
         if(!greska){
-            var data2 = JSON.stringify({naziv:naziv,tip:tip,pocetak:pocetak,kraj:kraj,dan:dan});
+            var data2 = JSON.stringify({
+                naziv: naziv + tip.naziv,
+                pocetak: pocetak,
+                kraj: kraj,
+                PredmetId: pr.id,
+                DanId: dan.id,
+                TipId: tip.id
+            });
             var xhr2 = new XMLHttpRequest();
             xhr2.withCredentials = true;
             xhr2.addEventListener("readystatechange", function() {
@@ -131,7 +148,7 @@ function unesiRaspored() {
                             console.log(this.responseText);
                           }
                         });
-                        xhr3.open("DELETE", "http://localhost:3000/v1/predmet/" + naziv);
+                        xhr3.open("DELETE", "http://localhost:3000/v2/predmet/" + pr.id);
                         xhr3.send();
                         alert("Greška! Neuspješno upisivanje aktivnosti!");
                     } else {
@@ -139,11 +156,11 @@ function unesiRaspored() {
                     }
                 }
             });
-            xhr2.open("POST", "http://localhost:3000/v1/aktivnost");
+            xhr2.open("POST", "http://localhost:3000/v2/aktivnost");
             xhr2.setRequestHeader("Content-Type", "application/json");
             xhr2.send(data2);
         }
-    }*/    
+    }
 }
 
 function pronadjiTip(id) {
@@ -159,6 +176,15 @@ function pronadjiDan(id) {
     for(let i = 0; i < dani.length; i++) {
         if(dani[i].id == id) {
             return dani[i];
+        }
+    }
+    return null;
+}
+
+function pronadjiPredmet(naziv) {
+    for(let i = 0; i < predmeti.length; i++) {
+        if(predmeti[i].naziv == naziv) {
+            return predmeti[i];
         }
     }
     return null;
