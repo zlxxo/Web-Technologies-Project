ed07a9fabd2f75286680de25ad703cbfd29b0493
zlxxo <mramic2@etf.unsa.ba>
2021-01-16 21:42:55 +0100

    inicijalizacija dana, tipova i predmeta

--- inicijalizacijaBaze.js
+++ inicijalizacijaBaze.js
@@ -2,10 +2,25 @@ const baza = require('./baza.js');
 
 const inicijalizacija = function() {
     // kreiranje baze
-    //baza.sequelize.sync({force: true});
-    baza.sequelize.sync().then(() => {
-        console.log("radi");
+    baza.sequelize.sync({force: true}).then(() => {
+        punjenjeBaze();
     });
+}
+
+function punjenjeBaze() {
+    const ponedjeljak = baza.Dan.create({naziv: "Ponedjeljak"});
+    const utorak = baza.Dan.create({naziv: "Utorak"});
+    const srijeda = baza.Dan.create({naziv: "Srijeda"});
+    const etvrtak = baza.Dan.create({naziv: "Cetvrtak"});
+    const petak = baza.Dan.create({naziv: "Petak"});
+
+    const predavanje = baza.Tip.create({naziv: "Predavanje"});
+    const vjezbe = baza.Tip.create({naziv: "Vjezbe"});
+    const tutorijal = baza.Tip.create({naziv: "Tutorijal"});
+
+    const wt = baza.Predmet.create({naziv: "WT"});
+    const rma = baza.Predmet.create({naziv: "RMA"});
+    const rpr = baza.Predmet.create({naziv: "RPR"});
 }
 
 const inicijalizacijaBaze = {};
